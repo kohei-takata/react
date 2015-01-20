8ab287fe0cb23c46dc153a5ea50f05bfb7e241d8
tako-black <gennarishock+tako@gmail.com>
2015-01-20 22:30:01 +0900

    Hook up the data model2

--- tutorial4.js
+++ tutorial4.js
@@ -15,11 +15,17 @@ var CommentBox = React.createClass({
  }
 });
 var CommentList = React.createClass({
- render: function() {          
+ render: function() {
+  var commentNodes = this.props.data.map(function (comment) {
+   return (
+    <Comment author={comment.author}>
+     {comment.text}
+    </Comment>
+   );
+  });
   return (
-   <div className="commentList">   
-    <Comment author="Pete Hunt">This is one comment</Comment>
-    <Comment author="Jordan Walke">This is *another* comment</Comment>
+   <div className="commentList">
+    {commentNodes}
    </div>
   );
  }                            
